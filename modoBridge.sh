f748d7185ee08116ec0d12af4033f78b1d7fd82f
René Solis <IoTeacher@users.noreply.github.com>
2020-02-22 10:48:06 -0800

    Update modoBridge.sh
    Now type ifconfig in the QEMU terminal, you must see a new IP address.
    
    Hope this helps someone. Thank you.

--- modoBridge.sh
+++ modoBridge.sh
@@ -9,3 +9,9 @@ sudo brctl addif br0 tap0
 sudo ip link set dev br0 up
 sudo ip link set dev tap0 up
 sudo dhclient br0
+
+
+echo %% MAC Address poner al final el numero de pc en hexadecimal, para no confundir el proxy del laboratorio %%
+
+  
+echo sudo qemu-system-arm -kernel kernel-qemu?dl=0 -cpu arm1176 -m 256 -M versatilepb -no-reboot -serial stdio -append "root=/dev/sda2 panic=1" -hda raspbian.img -net nic,macaddr=00:16:3e:00:00:01 -net tap,ifname=tap0,script=no,downscript=no -redir tcp:5022::22
