b0ca8d67b239735106a47478f8decd519c463fa8
René Solis <IoTeacher@users.noreply.github.com>
2020-10-28 22:43:28 -0700

    Create first.s

---
+++ arm32/codigo/first.s
@@ -0,0 +1,11 @@
+@ first.s
+@   from thinkingeek.com
+@   http://thinkingeek.com/2013/01/09/arm-assembler-raspberry-pi-chapter-1/
+@   Defines a main function that returns 2 as an exit code.
+@	
+
+	
+	.global main
+	
+main:   mov r0, #2       @ load immediate value 2 into Register r0 
+	bx lr            @ return 2 to Operating Sytem
