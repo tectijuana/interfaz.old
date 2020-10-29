c6163a1b3c0938288e91e6aa19cc1d7ae086eeae
René Solis <IoTeacher@users.noreply.github.com>
2020-10-29 14:40:07 -0700

    Create random.cpp

---
+++ arm32/codigo/random.cpp
@@ -0,0 +1,30 @@
+//
+//  Random.cpp
+//  Random-int
+//
+//  Created by Rohit Navalgund on 17/04/18.
+//  Copyright © 2018 Rohit Navalgund. All rights reserved.
+//
+
+// Uses C++'s inbuilt random() function to generate a random integer. This is a long integer (8bytes).
+#include <iostream>
+using namespace std;
+
+class Randomiser {
+public: long a; int n;
+
+    void calc() {
+        cout<<"How many random numbers do you want? ";
+        cin>>n;
+        for (int i = 0; i < n; i++) {
+            a = random();
+            cout<<a<<"\n";
+        }
+    }
+};
+
+int main() {
+    Randomiser r;
+    r.calc();
+    return 0;
+}
