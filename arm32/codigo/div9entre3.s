7ef7411e3d36f201299f2e0b3214c91955291803
René Solis <IoTeacher@users.noreply.github.com>
2020-11-24 23:25:15 -0800

    Create div9entre3.s

---
+++ arm32/codigo/div9entre3.s
@@ -0,0 +1,22 @@
+.global main
+.func main
+.arm
+
+main:    	ADR R5, thumbcode+1     //se asigna la dirección de la etiqueta thumbcode al registro 5
+		MOV LR, PC              //se almacena la siguiente instrucción en el registro LR
+		BX R5                   //salto al registro r5
+
+exit:   	MOV R0, R3
+		MOV R7, #1
+		SWI 0
+
+.thumb
+thumbcode:      MOV R0, #9     //Se almacena el valor del numerador
+		MOV R1, #3     //Se almacena el valor del denominador
+		MOV R3, #0     //Se utiliza como contador para saber cuantas veces cabe
+		
+loop:    	ADD R3, #1    //Se aumenta el contador en 1
+		SUB R0, R1    //Se resta valor del denominador al valor del numerador
+		BGE loop      //Se preguna se el resultado es un valor mayor o igual
+		SUB R3, #1    //Resultado de la división
+		BX LR	      //Retorno a al código principal
