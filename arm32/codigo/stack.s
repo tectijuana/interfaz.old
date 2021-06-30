db63c41614f846226d1f1b7d557ad66734e7c34d
René Solis <IoTeacher@users.noreply.github.com>
2021-06-30 14:56:54 -0700

    Update stack.s

--- arm32/codigo/stack.s
+++ arm32/codigo/stack.s
@@ -1,10 +1,23 @@
+//@@ Instituto Tecnologico de Tijuana
+//@@ Depto de Sistemas y Computación
+//@@ Ing.En Sistemas Computacionales
+
+//@@ Autor : Hernández Fernández Diana @DianaHFer
+//@@ Fecha de revisión : Miercoles 01 de Diciembre del 2020.
+
+
+//@ Objetivo del programa :
+
+Ejemplo muy sencillo  del uso del stack de la cpu.
+
 // Basically Push() and Pop() as in stacks. Hmm, interesting stuff for me as a beginner.
 
-.section __TEXT, __text
-.global _main
-.align 2
+//.section // __TEXT, __text
+.global main
 
-push {r7, lr}
-mov r1, 0xB
-pop{r7, pc}
-
+main:
+     mov   r0, #2  /* set up r0 */
+     push  {r0}    /* save r0 onto the stack */
+     mov   r0, #3  /* overwrite r0 */
+     pop   {r0}    /* restore r0 to it's initial state */
+     bx    lr      /* finish the program */
