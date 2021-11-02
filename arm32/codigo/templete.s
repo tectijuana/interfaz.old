5e51e623c88f367a392eb5533121b24dc0babec5
René Solis <IoTeacher@users.noreply.github.com>
2021-11-01 23:41:17 -0700

    Update templete.s

--- arm32/codigo/templete.s
+++ arm32/codigo/templete.s
@@ -1,9 +1,20 @@
-@ dummy.s
-@ a template for asm programs
+
+
+/*
+Title: Practica 0 Templete para programa ARM Assembly   
+Filename: hello.s
+Autor: ____Aqui su nombre___
+Date: ______
+Description : Hola Mundo en ARM Assembly
+Imput: -
+Ouput: HOLA MUNDO to console
+*/
+
+@ hello.s programa usa los recursos básicos
 .section	.data
 .section	.text
 .globl		_start
-_start:
+_start:		
 nop		@ no operation
 mov r7, $1	@ exit syscall
 svc $0		@ wake kernel
